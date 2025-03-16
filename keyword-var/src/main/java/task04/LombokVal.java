64739efefb831d90cd59ee8561a1561711ea5bf4
lkdcode <dlrlejr1@naver.com>
2025-03-16 17:21:11 +0900

    Feat(:var) lombok.val

---
+++ keyword-var/src/main/java/task04/LombokVal.java
@@ -0,0 +1,17 @@
+package task04;
+
+import lombok.val;
+
+public class LombokVal {
+
+    public void val() {
+        val num1 = 3;
+        val num2 = 6;
+        /*
+         num1 = 33;
+         num2 = 11;
+
+         */
+        System.out.println(num1 + num2);
+    }
+}
